87e4f56cfd0392c33425d90e98754c5dd27cabb7
Sudhindra Bhat <sudhindra.p.bhat@gmail.com>
2017-11-25 19:31:57 +0530

    supertwill

--- supertwill/supertwill.py
+++ supertwill/supertwill.py
@@ -23,6 +23,23 @@
 # A supervisor config snippet that tells supervisor to use this script
 # as a listener is below.
 
+#by default we assume env variables are set for credentials: TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN
+
+# https://www.twilio.com/docs/api/voice/making-calls
+
+# the url for twiml can be provided, so that it can be hosted by a simple webserver. This provides more control
+# to handle USER INPUT VIA KEYPAD (DTMF TONES), plus the webserver can also have hooks for status_callback
+
+# the url can have query params using which the webserver can determine what to say in the call
+
+#if url is not specified, by default we use twimlets, where you don't have to host your own webserver.
+
+#the caller has to specify the xml file name and we try to read it from PATH. also a config file with same name:
+#the config file has parameters like from no, to no, etc
+
+#we attach the xml to twimlets to place the call
+
+#if the files are not found we use a default xml to place the call.
 
 
 
@@ -34,6 +51,13 @@ import sys
 from superlance.process_state_monitor import ProcessStateMonitor
 from supervisor import childutils
 
+from twilio.rest import Client
+import time
+import urllib
+
+DEFAULT_CALL_URL = 'http://twimlets.com/message?%s'
+MAX_CALL_LIMIT = 1
+MAX_SMS_LIMIT = 3
 
 class SuperTwill(ProcessStateMonitor):
     process_state_events = ['PROCESS_STATE_FATAL']
@@ -98,7 +122,35 @@ class SuperTwill(ProcessStateMonitor):
         return txt
 
     def send_batch_notification(self):
-        pass
+        TWILIO_ACCOUNT_SID = ''
+        TWILIO_AUTH_TOKEN = ''
+        self.to_no = ''
+        self.from_no = ''
+        self.twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        i = 0
+        for msg in self.batchmsgs:
+            if self.mode in ['both', 'call'] and i < MAX_CALL_LIMIT:
+                self._alert_call_for_poloniex_new_coin(msg)
+            if self.mode in ['both', 'sms'] and i < MAX_SMS_LIMIT:
+                self._alert_message_for_poloniex_new_coin(msg)
+            i += 1
+
+
+    def _alert_message_for_poloniex_new_coin(self, msg):
+        #sentence_to_say = 'Alert msg'
+        message = self.twilio_client.messages.create(to=self.to_no,
+                           from_=self.from_no ,
+                           body=msg)
+
+    def _alert_call_for_poloniex_new_coin(self, msg):
+        #sentence_to_say = 'Alert msg'
+        f = {'Message': msg}
+        x = urllib.urlencode(f)
+        url = DEFAULT_CALL_URL % x
+
+        call = self.twilio_client.calls.create(to=self.to_no,
+                           from_=self.from_no ,
+                           url=url)
 
 
 
